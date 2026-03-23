3c5771d06258a946e1512020d09da678be4d71c5
rodrigo4hiro-create <rodrigo4hiro@gmail.com>
2026-03-23 15:58:35 +0900

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -22,9 +22,14 @@ export default async function handler(req, res) {
 
     const data = await response.json()
 
-    console.log(data)
+    let reply = "No response"
 
-  let reply = JSON.stringify(data)
+    if (data.output && data.output.length > 0) {
+      const item = data.output[0]
+      if (item.content && item.content.length > 0) {
+        reply = item.content[0].text
+      }
+    }
 
     return res.status(200).json({ reply })
 
